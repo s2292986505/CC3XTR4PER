7c97a99b823194ed60752e1cb6d3b4ac8c16c492
Izzeldin Addarda <38206617+zelsaddr@users.noreply.github.com>
2019-05-04 09:39:47 +0700

    Update cc.class.php

--- cc.class.php
+++ cc.class.php
@@ -1,3 +1,149 @@
-<?php <?php class creditCardGenerator{protected $bin;protected $message;protected $much;protected $check;public function color($color=base64_decode('ZGVmYXVsdA=='),$text){$arrayColor=array(base64_decode('Z3JleQ==') =>base64_decode('MTszMA=='),base64_decode('cmVk') =>base64_decode('MTszMQ=='),base64_decode('Z3JlZW4=') =>base64_decode('MTszMg=='),base64_decode('eWVsbG93') =>base64_decode('MTszMw=='),base64_decode('Ymx1ZQ==') =>base64_decode('MTszNA=='),base64_decode('cHVycGxl') =>base64_decode('MTszNQ=='),base64_decode('bmV2eQ==') =>base64_decode('MTszNg=='),base64_decode('d2hpdGU=') =>base64_decode('MTsw'),);return base64_decode('G1s=').$arrayColor[$color].base64_decode('bQ==').$text.base64_decode('G1swbQ==');}public function __construct($bin,$much,$check){$this->bin=$bin;$this->check=$check;if(is_numeric($check)){$this->check=$check;}else{echo $this->color(base64_decode('cmVk'),base64_decode('eyF9IENoZWNrIG11c3QgYm9vbGVhbgo='));}if(is_numeric($much)){$this->much=$much;}else{echo $this->color(base64_decode('cmVk'),base64_decode('eyF9IFRvdGFsIG11c3QgYmUgbnVtZXJpYyEK'));exit(1);}}protected function Save($title,$text){$fopen=fopen($title,base64_decode('YQ=='));fwrite($fopen,$text);fclose($fopen);}protected function Check($card){$ch=curl_init();$postData=http_build_query(array(base64_decode('YWpheA==') =>1,base64_decode('ZG8=') =>base64_decode('Y2hlY2s='),base64_decode('Y2NsaXN0') =>$card));$headers=array();$headers[]=base64_decode('Q29va2llOiBfZ2E9R0ExLjIuMjAzMTkwMzI2NC4xNTU1MzUzMzE0OyBmYz0lN0IlMjJOREkwZm1Gc1ltVmxZbUZpZVM1amIyMCUyMiUzQSUyMjElM0ExNTU1MzUzMzIwNjU4JTIyJTJDJTIyTkRJMGZtRjFkRzluY21Gd2FHSjFlUzVqYjIwJTIyJTNBJTIyMSUzQTE1NTUzNTMzMjA2OTQlMjIlMkMlMjJOREkwZm1wamNHVnVibVY1TG1OdmJRJTIyJTNBJTIyMiUzQTE1NTUzNTMzNDE5MzElMjIlN0Q7IHB2PSU3QiUyMmElMjIlM0ElMjIxJTNBMTU1NTM1MzMyMDY2NiUyMiUyQyUyMm8lMjIlM0ElMjIxJTNBMTU1NTM1MzMyMDcwMCUyMiUyQyUyMmQlMjIlM0ElMjIyJTNBMTU1NTM1MzM0MTkzNSUyMiU3RDsgUEhQU0VTU0lEPXBnbTdzbDN1cjRjbXA0OGFza2E1bjkxbG8z');$headers[]=base64_decode('T3JpZ2luOiBodHRwczovL21yY2hlY2tlci5uZXQ=');$headers[]=base64_decode('QWNjZXB0LUxhbmd1YWdlOiBlbi1VUyxlbjtxPTAuOQ==');$headers[]=base64_decode('VXNlci1BZ2VudDogTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgNi4xOyBXaW42NDsgeDY0KSBBcHBsZVdlYktpdC81MzcuMzYgKEtIVE1MLCBsaWtlIEdlY2tvKSBDaHJvbWUvNzMuMC4zNjgzLjEwMyBTYWZhcmkvNTM3LjM2');$headers[]=base64_decode('Q29udGVudC1UeXBlOiBhcHBsaWNhdGlvbi94LXd3dy1mb3JtLXVybGVuY29kZWQ7IGNoYXJzZXQ9VVRGLTg=');$headers[]=base64_decode('QWNjZXB0OiBhcHBsaWNhdGlvbi9qc29uLCB0ZXh0L2phdmFzY3JpcHQsICovKjsgcT0wLjAx');$headers[]=base64_decode('UmVmZXJlcjogaHR0cHM6Ly9tcmNoZWNrZXIubmV0L2NhcmQvY2NuMy8=');$headers[]=base64_decode('WC1SZXF1ZXN0ZWQtV2l0aDogWE1MSHR0cFJlcXVlc3Q=');$headers[]=base64_decode('Q29ubmVjdGlvbjoga2VlcC1hbGl2ZQ==');$options=array(CURLOPT_URL =>base64_decode('aHR0cHM6Ly9tcmNoZWNrZXIubmV0L2NhcmQvY2NuMy9hbGllbjA3LnBocA=='),CURLOPT_RETURNTRANSFER =>true,CURLOPT_POST =>true,CURLOPT_POSTFIELDS =>$postData,CURLOPT_HTTPHEADER =>$headers,CURLOPT_FOLLOWLOCATION =>true);curl_setopt_array($ch,$options);$exec=curl_exec($ch);return $exec;}public function getCC(){if(($this->bin <=0)||!isset($this->bin)){$message[0]=base64_decode('U2V0IGJpbiBmaXJzdA==');echo $this->color(base64_decode('eWVsbG93'),$message);}else if($this->bin>9999999999999999){$message[0]=base64_decode('QmluIHRvIGxvbmc=');echo $this->color(base64_decode('eWVsbG93'),$message);}else{$output=$this->ccNumber($this->bin,16,$this->much);return $output;}}protected function generateYears(){$randDate=rand(1,30);$randYears=rand(20,25);$randCvv=rand(010,800);$randDate<10?$randDate=base64_decode('MA==').$randDate:$randDate=$randDate;$randCvv<100?$randCvv=base64_decode('MA==').$randCvv:$randCvv=$randCvv;return base64_decode('fA==').$randDate.base64_decode('fDIw').$randYears.base64_decode('fA==').$randCvv;}protected function completedNumber($prefix,$length){$ccnumber=$this->bin;while(strlen($ccnumber)<($length-1)){$ccnumber.=rand(0,9);}# Calculate sum
- $sum=0;$pos=0;$reversedCCnumber=strrev($ccnumber);while($pos<$length-1){$odd=$reversedCCnumber[$pos]*2;if($odd>9){$odd-=9;}$sum+=$odd;if($pos!=($length-2)){$sum+=$reversedCCnumber[$pos+1];}$pos+=2;}# Calculate check digit
- $checkdigit=((floor($sum/10)+1)*10-$sum)%10;$ccnumber.=$checkdigit;return $ccnumber;}protected function ccNumber($prefixList,$length,$howMany){for($i=0;$i<$howMany;$i++){if($this->check ==true){$card=$this->completedNumber($this->bin,$length).$this->generateYears();$check=json_decode($this->Check($card));if($check->error ==0){preg_match_all(base64_decode('Izxmb250IGNvbG9yPWdyZWVuPiguKj8pPC9mb250PiNzaQ=='),$check->msg,$charge);echo $card.$this->color(base64_decode('Z3JlZW4='),base64_decode('ID4+IExJVkUgWyBDaGFyZ2UgOiA=').$charge[1][1].base64_decode('IF0K'));$this->Save(base64_decode('UmVzdWx0LQ==').$this->bin.base64_decode('LnR4dA=='),$card.base64_decode('IHwg').$charge[1][1].base64_decode('Cg=='));}else if($check->error ==2){echo $card.$this->color(base64_decode('cmVk'),base64_decode('ID4+IERJRQo='));}else{echo $card.$this->color(base64_decode('Z3JleQ=='),base64_decode('ID4+IFVOS05PV04K'));}}else{echo $this->completedNumber($this->bin,$length).$this->generateYears().base64_decode('Cg==');}}}}?>
+<?php
+class creditCardGenerator
+{
+    protected $bin;
+    protected $message;
+    protected $much;
+    protected $check;
+    
+    public function color($color = "default" , $text){
+    	$arrayColor = array(
+    		'grey' 		=> '1;30',
+    		'red' 		=> '1;31',
+    		'green' 	=> '1;32',
+    		'yellow' 	=> '1;33',
+    		'blue' 		=> '1;34',
+    		'purple' 	=> '1;35',
+    		'nevy' 		=> '1;36',
+    		'white' 	=> '1;0',
+    	);	
+    	return "\033[".$arrayColor[$color]."m".$text."\033[0m";
+    }
+    public function __construct($bin, $much, $check){
+        $this->bin = $bin;
+        $this->check = $check;
+        if (is_numeric($check)) {
+            $this->check = $check;
+        }else{
+            echo $this->color("red", "{!} Check must boolean\n");
+        }
+        if (is_numeric($much)) {
+            $this->much = $much;
+        } else {   
+            echo $this->color("red", "{!} Total must be numeric!\n");
+            exit(1);
+        }
+    }
+    protected function Save($title, $text){
+        $fopen = fopen($title, "a");
+        fwrite($fopen, $text);
+        fclose($fopen);
+    }
+    protected function Check($card){
+        $ch = curl_init();
+        $postData = http_build_query(
+                        array(
+                                "ajax" => 1,
+                                "do" => "check",
+                                "cclist" => $card
+                            )
+                    );
+        $headers = array();
+        $headers[] = 'Cookie: _ga=GA1.2.2031903264.1555353314; fc=%7B%22NDI0fmFsYmVlYmFieS5jb20%22%3A%221%3A1555353320658%22%2C%22NDI0fmF1dG9ncmFwaGJ1eS5jb20%22%3A%221%3A1555353320694%22%2C%22NDI0fmpjcGVubmV5LmNvbQ%22%3A%222%3A1555353341931%22%7D; pv=%7B%22a%22%3A%221%3A1555353320666%22%2C%22o%22%3A%221%3A1555353320700%22%2C%22d%22%3A%222%3A1555353341935%22%7D; PHPSESSID=pgm7sl3ur4cmp48aska5n91lo3';
+        $headers[] = 'Origin: https://mrchecker.net';
+        $headers[] = 'Accept-Language: en-US,en;q=0.9';
+        $headers[] = 'User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36';
+        $headers[] = 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8';
+        $headers[] = 'Accept: application/json, text/javascript, */*; q=0.01';
+        $headers[] = 'Referer: https://mrchecker.net/card/ccn3/';
+        $headers[] = 'X-Requested-With: XMLHttpRequest';
+        $headers[] = 'Connection: keep-alive';
+        $options = array(
+                        CURLOPT_URL => 'https://mrchecker.net/card/ccn3/alien07.php',
+                        CURLOPT_RETURNTRANSFER => true,
+                        CURLOPT_POST => true, 
+                        CURLOPT_POSTFIELDS => $postData,
+                        CURLOPT_HTTPHEADER => $headers,
+                        CURLOPT_FOLLOWLOCATION => true
+                );
+        curl_setopt_array($ch, $options);
+        $exec = curl_exec($ch);
+        return $exec;
+    }
+    public function getCC() 
+    {
+        if (($this->bin <= 0) || !isset($this->bin)) {
+            $message[0] = 'Set bin first';
+            echo $this->color("yellow", $message);
+        } else if ($this->bin > 9999999999999999) {
+            $message[0] = 'Bin to long';
+            echo $this->color("yellow", $message);
+        } else {
+            $output = $this->ccNumber($this->bin, 16, $this->much);
+            return $output;
+        }
+    }
+    protected function generateYears(){
+        $randDate = rand(1,30);
+        $randYears = rand(20,25);
+        $randCvv = rand(010, 800);
+        $randDate < 10 ? $randDate = "0".$randDate : $randDate = $randDate;
+        $randCvv < 100 ? $randCvv = "0".$randCvv : $randCvv = $randCvv;
+        return "|".$randDate."|20".$randYears."|".$randCvv;
+    }
+    protected function completedNumber($prefix, $length)
+    {
+        $ccnumber = $this->bin;
+    
+        while ( strlen($ccnumber) < ($length - 1) ) {
+            $ccnumber .= rand(0,9);
+        }
+    
+        # Calculate sum
+        $sum = 0;
+        $pos = 0;
+        $reversedCCnumber = strrev( $ccnumber );
+    
+        while ( $pos < $length - 1 ) {
+            $odd = $reversedCCnumber[ $pos ] * 2;
+            if( $odd > 9 ) {
+                $odd -= 9;
+            }
+            $sum += $odd;
+    
+            if( $pos != ($length - 2) ) {
+    
+                $sum += $reversedCCnumber[ $pos +1 ];
+            }
+            $pos += 2;
+        }
+    
+        # Calculate check digit
+        $checkdigit = (( floor($sum/10) + 1) * 10 - $sum) % 10;
+        $ccnumber .= $checkdigit;
+        return $ccnumber;
+    }
+    
+    
+    protected function ccNumber($prefixList, $length, $howMany) 
+    {
+        for ($i = 0; $i < $howMany; $i++) {
+            if ($this->check == true) {
+                $card = $this->completedNumber($this->bin, $length).$this->generateYears();
+                $check = json_decode($this->Check($card));
+                if ($check->error == 0){
+                    preg_match_all('#<font color=green>(.*?)</font>#si', $check->msg, $charge);
+                    echo $card.$this->color("green", " >> LIVE [ Charge : ".$charge[1][1]." ]\n");
+                    $this->Save("Result-".$this->bin.".txt", $card." | ".$charge[1][1]."\n");
+                }else if ($check->error == 2){
+                    echo $card.$this->color("red", " >> DIE\n");
+                } else {
+                    echo $card.$this->color("grey", " >> UNKNOWN\n");
+                }
+            } else {
+                echo $this->completedNumber($this->bin, $length).$this->generateYears()."\n";
+            }
+        }
+    }
+
+}

bd2f1404313c5fcc6f6f9289046c7b6b342ed2dd
Izzeldin Addarda <38206617+zelsaddr@users.noreply.github.com>
2019-05-07 10:22:53 +0700

    Update run.php

--- run.php
+++ run.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * @package CC Extraper & Checker
- * @author jacek-9999, radenvodka, zelsaddr
+ * @author fb.com/www.zeldin.go.id
+ * @package CC GENERATOR
  * Thanks to https://github.com/radenvodka/color-php for PHP Color Function
  * Thanks to https://github.com/jacek-9999/credit-card-generator/ 
  * Iam Recode the cc.class.php from jacek-9999 in github'
@@ -9,13 +9,13 @@
  *             - Adding feature Year,
  *             - Adding feature CVV,
  *             - Adding feature Checker for Live card dll
- * NOTE : FILE NAME SAVED AS YOUR BIN INPUT.
- * MAZTERIN.COM
+ * NOTE : FILE NAME SAVED WITH YOUR BIN INPUT.
 **/
+
 require "./cc.class.php";
-echo ">> BIN : "; $bin = trim(fgets(STDIN)); // BIN
-echo ">> Total : "; $total = trim(fgets(STDIN)); // TOTAL in Numeric
-echo ">> Check : "; $check = trim(fgets(STDIN)); // BOOLEAN 1 or 0
-$cc = new creditCardGenerator($bin, $total, $check);
-$cc->getCC();
+echo ">> BIN : "; $bin = trim(fgets(STDIN));
+echo ">> Check ? 1/0 : "; $check = trim(fgets(STDIN));
+echo ">> Amount : "; $amount = trim(fgets(STDIN));
+$new = new CC($bin, $check, $amount);
+echo $new->Execute();
 ?>
